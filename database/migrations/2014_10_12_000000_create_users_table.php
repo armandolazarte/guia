6908dc9fff849a9c87bbc2d77b47db25612ae804
Samuel Mercado <samuel.mg@gmx.com>
2015-02-10 02:31:54 -0600

    Se agregan campos a users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,19 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('name');
-			$table->string('email')->unique();
+
+			$table->string('username', 20)->unique();
+			$table->string('email', 128)->unique();
 			$table->string('password', 60);
+
+			$table->string('nombre');
+			$table->string('cargo');
+			$table->string('prefijo', 5);
+			$table->string('iniciales', 5);
+
+			$table->boolean('active');
+			$table->boolean('validated');
+
 			$table->rememberToken();
 			$table->timestamps();
 		});
