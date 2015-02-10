6ed151a5a5632a03b1a03bfc37e9475b4b9f5a7d
Samuel Mercado <samuel.mg@gmx.com>
2015-02-09 23:20:31 -0600

    Migración para tablas sol depósitos

---
+++ database/migrations/2015_02_10_051829_crear_tablas_sol_depositos.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablasSolDepositos extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('sol_depositos', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->date('fecha');
+			$table->string('estatus', 20);
+			$table->integer('fondo_id')->unsigned();
+			$table->foreign('fondo_id')->references('id')->on('fondos');
+			$table->integer('afin_soldep')->unsigned();
+			$table->timestamps();
+		});
+
+		Schema::create('sol_depositos_docs', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('sol_deposito_id')->unsigned();
+			$table->foreign('sol_deposito_id')->references('id')->on('sol_depositos');
+			$table->integer('doc_id')->unsigned();
+			$table->string('doc_type');
+			$table->decimal('monto', 15, 3);
+			$table->decimal('monto_retencion', 15, 3);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('sol_depositos_docs');
+		Schema::drop('sol_depositos');
+	}
+
+}
