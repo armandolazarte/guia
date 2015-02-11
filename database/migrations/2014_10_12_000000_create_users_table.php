bac23f2c0495099b88c032e2503ea418da8a739d
Samuel Mercado <samuel.mg@gmx.com>
2015-02-11 13:15:22 -0600

    Se modifica tamaño de email en migración para tabla users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration {
 			$table->increments('id');
 
 			$table->string('username', 20)->unique();
-			$table->string('email', 128)->unique();
+			$table->string('email')->unique();
 			$table->string('password', 60);
 
 			$table->string('nombre');
