631d97b88a190551dffcd13ee1a8af395e871f0e
Samuel Mercado <samuel.mg@gmx.com>
2015-02-13 17:02:09 -0600

    Se especifica título en layout app. Se extrae container principal.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Laravel</title>
+	<title>GUIA :: Gestor Universitario de Información y Administración</title>
 
 	<link href="/css/app.css" rel="stylesheet">
 
@@ -53,7 +53,9 @@
 		</div>
 	</nav>
 
+    <div class="container-fluid">
 	@yield('content')
+    </div>
 
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
