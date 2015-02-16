7ca45976fd1cdfa892bd5778559b2762faeca0ec
Samuel Mercado <samuel.mg@gmx.com>
2015-02-16 12:01:59 -0600

    Se elimina pivote cargo_urg. Se agrega urg_id en tabla cargos.

--- database/migrations/2015_02_10_084445_crear_tablas_cargos.php
+++ database/migrations/2015_02_10_084445_crear_tablas_cargos.php
@@ -17,18 +17,12 @@ class CrearTablasCargos extends Migration {
 			$table->increments('id');
 			$table->integer('user_id')->unsigned();
 			$table->foreign('user_id')->references('id')->on('users');
+            $table->integer('urg_id')->unsigned();
+            $table->foreign('urg_id')->references('id')->on('urgs');
 			$table->date('inicio');
 			$table->date('fin');
 		});
 
-		Schema::create('cargo_urg', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->integer('cargo_id')->unsigned();
-			$table->foreign('cargo_id')->references('id')->on('cargos');
-			$table->integer('urg_id')->unsigned();
-			$table->foreign('urg_id')->references('id')->on('urgs');
-		});
 	}
 
 	/**
@@ -38,7 +32,6 @@ class CrearTablasCargos extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('cargo_urg');
 		Schema::drop('cargos');
 	}
 
