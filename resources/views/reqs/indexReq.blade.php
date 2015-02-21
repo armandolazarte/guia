3d5de51b401db9a6cd533a5502ef92f9d2ad74cd
Samuel Mercado <samuel.mg@gmx.com>
2015-02-20 23:48:19 -0600

    Actualización de vista indexReq para implementar bootstrap

--- resources/views/reqs/indexReq.blade.php
+++ resources/views/reqs/indexReq.blade.php
@@ -1,41 +1,42 @@
-@extends('layouts.base')
+@extends('app')
 
-@section('contenido')
-
-@if(isset($reqs))
-    <table border="1">
-        <thead>
-        <tr>
-            <th>Requisición</th>
-            <th>Fecha</th>
-            <th>Unidad Responsable</th>
-            <th>Etiqueta</th>
-            <th>Estatus</th>
-            <th>Monto</th>
-            <th>Orden de Compra</th>
-            <th>Fecha OC</th>
-            <th>Estatus OC</th>
-            <th>Responsable Adq.</th>
-        </tr>
-        </thead>
-        @foreach($reqs as $req)
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+    @if(isset($reqs))
+        <table class="table-hover">
+            <thead>
             <tr>
-                <td><a href="{{ action('RequisicionController@show', array($req->id)) }}">{{ $req->req }}</a></td>
-                <td>{{ $req->fecha_req }}</td>
-                <td>{{ $req->urg->d_urg }}</td>
-                <td>{{ $req->etiqueta }}</td>
-                <td>{{ $req->estatus }}</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <th>Requisición</th>
+                <th>Fecha</th>
+                <th>Unidad Responsable</th>
+                <th>Etiqueta</th>
+                <th>Estatus</th>
+                <th>Monto</th>
+                <th>Orden de Compra</th>
+                <th>Fecha OC</th>
+                <th>Estatus OC</th>
+                <th>Responsable Adq.</th>
             </tr>
-        @endforeach
-    </table>
-@else
-    <h3>No hay requisiciones que mostrar</h3>
-@endif
-
-
+            </thead>
+            @foreach($reqs as $req)
+                <tr>
+                    <td><a href="{{ action('RequisicionController@show', array($req->id)) }}">{{ $req->req }}</a></td>
+                    <td>{{ $req->fecha_req }}</td>
+                    <td>{{ $req->urg->d_urg }}</td>
+                    <td>{{ $req->etiqueta }}</td>
+                    <td>{{ $req->estatus }}</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            @endforeach
+        </table>
+    @else
+        <h3>No hay requisiciones que mostrar</h3>
+    @endif
+    </div>
+</div>
 @stop
