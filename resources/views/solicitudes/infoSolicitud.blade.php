40ac23cd908362316f0b3bf6c4a244a72f01bcb2
Samuel Mercado <samuel.mg@gmx.com>
2015-03-04 15:47:51 -0600

    Formularios para enviar o recuperar solicitud

--- resources/views/solicitudes/infoSolicitud.blade.php
+++ resources/views/solicitudes/infoSolicitud.blade.php
@@ -18,6 +18,20 @@
             @if($solicitud->estatus == "")
                 <a href="{{ action('SolicitudRecursosController@create', array($solicitud->id)) }}" class="btn btn-primary">Agregar Recursos</a>
             @endif
+
+            @if($solicitud->estatus == '')
+                {!! Form::open(array('action' => ['SolicitudController@update', $solicitud->id], 'method' => 'patch', 'class' => 'form')) !!}
+                    <input type="hidden" name="accion" value="Enviar">
+                    <button type="submit" class="btn btn-success">Enviar a Finanzas</button>
+                {!! Form::close() !!}
+            @endif
+
+            @if($solicitud->estatus == 'Enviada')
+                    {!! Form::open(array('action' => ['SolicitudController@update', $solicitud->id], 'method' => 'patch', 'class' => 'form')) !!}
+                    <input type="hidden" name="accion" value="Recuperar">
+                    <button type="submit" class="btn btn-warning">Recuperar</button>
+                {!! Form::close() !!}
+            @endif
         </div>
     </div>
 @stop
