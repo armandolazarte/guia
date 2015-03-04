cc11cf420c2bd88a7d70ce708b1c5f22f72f8174
Samuel Mercado <samuel.mg@gmx.com>
2015-03-04 15:44:45 -0600

    Corrección de namespace: Guia\Models. Declaración $fillable = []

--- app/Models/Registro.php
+++ app/Models/Registro.php
@@ -1,10 +1,12 @@
-<?php namespace Guia;
+<?php namespace Guia\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Registro extends Model {
 
     public $timestamps = false;
+
+    protected $fillable = ['user_id', 'estatus', 'fecha_hora'];
 
     //Registro __belongs_to__ User
     public function user()
