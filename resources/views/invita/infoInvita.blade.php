572c1646a7cda34e66785cbb3781af510ee4fae1
Samuel Mercado <samuel.mg@gmx.com>
2015-04-27 12:42:29 -0500

    Corrección de infoInvita.blade

--- resources/views/invita/infoInvita.blade.php
+++ resources/views/invita/infoInvita.blade.php
@@ -3,27 +3,24 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            @if(count($cotizacion) > 0)
-                <table>
+            @if(isset($cotizacion))
+                <table class="table table-condensed">
                     <thead>
                     <tr>
                         <th>Beneficiario</th>
                         <th>Fecha de Invitación</th>
                     </tr>
                     </thead>
-                    @foreach($cotizaciones as $cotiza)
-                        <tr>
-                            <td>{{ $cotiza->benef->d_benef }}</td>
-                            <td>{{ $cotiza->fecha_invitacion }}</td>
-                        </tr>
-                    @endforeach
+                    <tr>
+                        <td>{{ $cotizacion->benef->benef }}</td>
+                        <td>{{ $cotizacion->fecha_invitacion }}</td>
+                    </tr>
                 </table>
             @else
                 <div class="alert alert-info">
                     No hay invitaciones capturadas
                 </div>
             @endif
-            <a href="{{ action('InvitacionController@create', $req_id) }}" class="btn btn-success">Agregar Invitación</a>
         </div>
     </div>
 @stop
