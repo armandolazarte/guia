f6d87244d9dff5c34f4db734572254bc0dd6dda3
Samuel Mercado <samuel.mg@gmx.com>
2015-04-27 09:32:25 -0500

    Creación de InvitacionRequest

---
+++ app/Http/Requests/InvitacionRequest.php
@@ -0,0 +1,31 @@
+<?php namespace Guia\Http\Requests;
+
+use Guia\Http\Requests\Request;
+
+class InvitacionRequest extends Request {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			'benef_id' => 'required|integer',
+            'fecha_invitacion' => 'required|date',
+            'req_id' => 'required|integer'
+		];
+	}
+
+}
