a9634944502ade41933a7dcefb614546a534c6a7
Samuel Mercado <samuel.mg@gmx.com>
2015-04-30 16:13:50 -0500

    Métodos store y show en MatrizCuadroController

--- app/Http/Controllers/MatrizCuadroController.php
+++ app/Http/Controllers/MatrizCuadroController.php
@@ -2,9 +2,11 @@
 
 use Guia\Http\Requests;
 use Guia\Http\Controllers\Controller;
+use Guia\Http\Requests\MatrizCuadroRequest;
 
 use Guia\Models\Articulo;
 use Guia\Models\Cotizacion;
+use Guia\Models\Cuadro;
 use Illuminate\Http\Request;
 
 class MatrizCuadroController extends Controller {
@@ -37,9 +39,47 @@ class MatrizCuadroController extends Controller {
 	 *
 	 * @return Response
 	 */
-	public function store()
+	public function store(MatrizCuadroRequest $request)
 	{
-		//
+        $req_id = $request->input('req_id');
+
+        //Crear Cuadro
+        $cuadro = new Cuadro();
+        $cuadro->req_id = $req_id;
+        $cuadro->fecha_cuadro = \Carbon\Carbon::now()->toDateString();
+        $cuadro->estatus = 'Cotizando';
+        $cuadro->elabora = \Auth::user()->id;
+        $cuadro->save();
+
+        //Insertar en articulo_cotizacion
+        $arr_articulos_id = Articulo::whereReqId($req_id)->lists('id');
+        $arr_cotizaciones_id = Cotizacion::whereReqId($req_id)->lists('id');
+
+        foreach($arr_articulos_id as $articulo_id){
+            $articulo = Articulo::find($articulo_id);
+
+            foreach($arr_cotizaciones_id as $cotizacion_id){
+                $costo = $request->input('costo_'.$articulo_id.'_'.$cotizacion_id);
+                $sel = $request->input('sel_'.$articulo_id.'_'.$cotizacion_id);
+                $articulo->cotizaciones()->attach([$cotizacion_id => ['costo' => $costo, 'sel' => $sel]]);
+            }
+
+            //Actualizar impuesto en articulos
+            $articulo->impuesto = $request->input('impuesto_'.$articulo_id);
+        }
+
+        //Actualizar fecha_cotiza en cotizaciones
+        foreach($arr_cotizaciones_id as $cotizacion_id) {
+            $cotizacion = Cotizacion::find($cotizacion_id);
+            $cotizacion->cuadro_id = $cuadro->id;
+            $cotizacion->fecha_cotiza = \Carbon\Carbon::now()->toDateString();
+            $cotizacion->vigencia = $request->input('vigencia_'.$cotizacion_id);
+            $cotizacion->garantia = $request->input('garantia'.$cotizacion_id);
+            $cotizacion->imprimir = true;
+            $cotizacion->save();
+        }
+
+        return redirect()->action('MatrizCuadroController@show', array($req_id));
 	}
 
 	/**
@@ -48,9 +88,12 @@ class MatrizCuadroController extends Controller {
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function show($id)
+	public function show($req_id)
 	{
-		//
+        $cotizaciones = Cotizacion::whereReqId($req_id)->get();
+        $articulos = Articulo::whereReqId($req_id)->get();
+
+        return view('cuadro.matrizCuadro', compact('req_id', 'cotizaciones', 'articulos'));
 	}
 
 	/**
