decab70fd1351a02e67b750d8b9b9451f34bd6fd
Samuel Mercado <samuel.mg@gmx.com>
2015-06-03 11:37:50 -0500

    Se agregan roles Cotizador, Ejecutora y Comprobación a Seeder

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -15,11 +15,14 @@ class RolesTableSeeder extends Seeder {
         Role::create(array('role_name' => 'Administrador'));
         Role::create(array('role_name' => 'Usuario'));
         Role::create(array('role_name' => 'Adquisiciones'));
+        Role::create(array('role_name' => 'Cotizador'));
         Role::create(array('role_name' => 'Patrimonio'));
         Role::create(array('role_name' => 'Directivo'));
         Role::create(array('role_name' => 'Recepción'));
         Role::create(array('role_name' => 'Presupuesto'));
+        Role::create(array('role_name' => 'Ejecutora'));
         Role::create(array('role_name' => 'Contabilidad'));
+        Role::create(array('role_name' => 'Comprobacion'));
         Role::create(array('role_name' => 'Bancos'));
         Role::create(array('role_name' => 'Fondos Externos'));
         Role::create(array('role_name' => 'DMCYP - DIP'));
