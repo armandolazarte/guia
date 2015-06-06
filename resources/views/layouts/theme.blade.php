b362b247603c89ec0fff050887d4d03b79172d51
Samuel Mercado <samuel.mg@gmx.com>
2015-06-05 19:53:38 -0500

    Se agregan secciones css y js en tema principal

--- resources/views/layouts/theme.blade.php
+++ resources/views/layouts/theme.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>GUIA :: Gestor Universitario de Información y Administración</title>
 
+    @section('css')
     <link href="/css/app.css" rel="stylesheet">
 
     <!-- Fonts -->
@@ -17,6 +18,7 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    @show
 </head>
 <body>
 <nav class="navbar navbar-default">
@@ -58,8 +60,10 @@
     @yield('content')
 </div>
 
+@section('js')
 <!-- Scripts -->
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+@show
 </body>
 </html>
