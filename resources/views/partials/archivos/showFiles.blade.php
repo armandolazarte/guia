45ad310bd4537f36c712c2daba11b18d761066b2
Samuel Mercado <samuel.mg@gmx.com>
2015-06-24 09:24:05 -0500

    Formato showFiles.blade y se deshabilita enlace para descarga de archivos

--- resources/views/partials/archivos/showFiles.blade.php
+++ resources/views/partials/archivos/showFiles.blade.php
@@ -2,10 +2,12 @@
     <table class="table table-condensed">
         @foreach($archivos as $archivo)
             <tr>
-                <td><a href="{{ action('ArchivosController@descargar', $presupuesto.'/'.$archivo->id) }}">{{ $archivo->name }}</a></td>
+                <td><a href="#{{-- action('ArchivosController@descargar', $presupuesto.'/'.$archivo->id) --}}">{{ $archivo->name }}</a></td>
             </tr>
         @endforeach
     </table>
 @else
-    No hay archivos cargados
+    <div class="alert alert-warning" role="alert">
+        <p class="text-center"><b>No hay archivos cargados</b></p>
+    </div>
 @endif
