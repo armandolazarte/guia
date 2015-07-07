5b0c9d6a874f917578400e533078f45a302f8076
Samuel Mercado <samuel.mg@gmx.com>
2015-07-06 20:40:36 -0500

    Se agrega "Fondos Externos" a TipoProyectosTableSeeder

--- database/seeds/TipoProyectosTableSeeder.php
+++ database/seeds/TipoProyectosTableSeeder.php
@@ -12,5 +12,6 @@ class TipoProyectosTableSeeder extends Seeder {
 
         TipoProyecto::create(array('tipo_proyecto' => 'P3E-LGCG'));
         TipoProyecto::create(array('tipo_proyecto' => 'P3E 2011 y Anteriores'));
+        TipoProyecto::create(array('tipo_proyecto' => 'Fondos Externos'));
     }
 }
