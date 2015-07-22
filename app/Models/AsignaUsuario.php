fe156617d3b8738ab83082a2e9e2fc5993794aac
Samuel Mercado <samuel.mg@gmx.com>
2015-07-22 17:22:23 -0500

    Declaración de $fillable en modelo AsignaUsuario

--- app/Models/AsignaUsuario.php
+++ app/Models/AsignaUsuario.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class AsignaUsuario extends Model
 {
     public $timestamps = false;
+    protected $fillable = ['user_id','asignado_user_id'];
 
     public function usuarioAsigna()
     {
