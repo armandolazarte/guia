df214546cb01a195180156765dd7d317c7972ec6
Samuel Mercado <samuel.mg@gmx.com>
2015-07-28 20:32:43 -0500

    Ajustes en modelos RelInterna y RelInternaDoc
    Se declara $fillable en ambos modelos.
    Se declara relación polimórfica en modelo RelInternaDoc

--- app/Models/RelInterna.php
+++ app/Models/RelInterna.php
@@ -1,8 +1,13 @@
-<?php namespace Guia\Models;
+<?php
+
+namespace Guia\Models;
+
 
 use Illuminate\Database\Eloquent\Model;
 
 class RelInterna extends Model {
+
+    protected $fillable = ['fecha_envio','fecha_revision','envia','recibe','estatus','tipo_documentos'];
 
     //RelInterna __has_many__ RelInternaDoc
     public function relInternaDocs()
--- app/Models/RelInternaDoc.php
+++ app/Models/RelInternaDoc.php
@@ -1,15 +1,25 @@
-<?php namespace Guia\Models;
+<?php
+
+namespace Guia\Models;
+
 
 use Illuminate\Database\Eloquent\Model;
 
 class RelInternaDoc extends Model {
 
+    protected $fillable = ['rel_interna_id','validacion','docable_id','docable_type'];
     public $timestamps = false;
 
     //RelInternaDoc __belongs_to__ RelInterna
     public function relInterna()
     {
         return $this->belongsTo('Guia\Models\RelInterna');
+    }
+
+    //RelInternaDoc __morph_to__ Egreso
+    public function docable()
+    {
+        return $this->morphTo();
     }
 
 }
