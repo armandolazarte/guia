87d0c0351aebf334060084a941a7f70e0b91e65d
Samuel Mercado <samuel.mg@gmx.com>
2015-08-20 15:48:31 -0500

    Migración para tabla pivote "egreso_proyecto"

---
+++ database/migrations/2015_08_20_172911_CrearTablaEgresoProyecto.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaEgresoProyecto extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('egreso_proyecto', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('egreso_id')->unsigned();
+            $table->foreign('egreso_id')->references('id')->on('egresos')->onDelete('cascade');
+            $table->integer('proyecto_id')->unsigned();
+            $table->foreign('proyecto_id')->references('id')->on('proyectos')->onDelete('cascade');
+            $table->decimal('monto', 15, 3);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('egreso_proyecto');
+    }
+}
