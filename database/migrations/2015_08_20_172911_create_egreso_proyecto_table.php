5dec53fef45d874799d05fc6d0446c614b38b031
Samuel Mercado <samuel.mg@gmx.com>
2015-08-26 00:25:01 -0500

    Estandarización en nombre de migración de la clase CreateEgresoProyectoTable

--- database/migrations/2015_08_20_172911_create_egreso_proyecto_table.php
+++ database/migrations/2015_08_20_172911_create_egreso_proyecto_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CrearTablaEgresoProyecto extends Migration
+class CreateEgresoProyectoTable extends Migration
 {
     /**
      * Run the migrations.
