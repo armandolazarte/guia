2aaf3f2744ec2eeb2bb9f2d9cad4c32a5b82bf8b
Samuel Mercado <samuel.mg@gmx.com>
2015-09-18 13:25:31 -0500

    Se agregan FK contraints a tablas relacionadas con ingresos y egresos
    FK contraints a tablas ingreso_rm, egreso_rm, egreso_oc y egreso_solicitud
    Se aplica formato a los archivos modificados

--- database/migrations/2015_02_10_070632_crear_tablas_adquisiciones.php
+++ database/migrations/2015_02_10_070632_crear_tablas_adquisiciones.php
@@ -3,172 +3,164 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CrearTablasAdquisiciones extends Migration {
+class CrearTablasAdquisiciones extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('reqs', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->integer('req')->unsigned()->unique();
-			$table->date('fecha_req');
-			$table->integer('urg_id')->unsigned();
-			$table->foreign('urg_id')->references('id')->on('urgs');
-			$table->integer('proyecto_id')->unsigned();
-			$table->foreign('proyecto_id')->references('id')->on('proyectos');
-			$table->string('etiqueta', 100);
-			$table->string('lugar_entrega');
-			$table->text('obs');
-			$table->smallInteger('solicita')->unsigned();
-			$table->smallInteger('autoriza')->unsigned();
-			$table->smallInteger('vobo')->unsigned();
-			$table->string('estatus', 20);
-			$table->integer('user_id')->unsigned();
-			$table->decimal('tipo_cambio', 9, 6);
-			$table->string('moneda', 10);
-			$table->string('tipo_orden', 20);
-			$table->timestamps();
-			$table->softDeletes();
-		});
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reqs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('req')->unsigned()->unique();
+            $table->date('fecha_req');
+            $table->integer('urg_id')->unsigned();
+            $table->foreign('urg_id')->references('id')->on('urgs');
+            $table->integer('proyecto_id')->unsigned();
+            $table->foreign('proyecto_id')->references('id')->on('proyectos');
+            $table->string('etiqueta', 100);
+            $table->string('lugar_entrega');
+            $table->text('obs');
+            $table->smallInteger('solicita')->unsigned();
+            $table->smallInteger('autoriza')->unsigned();
+            $table->smallInteger('vobo')->unsigned();
+            $table->string('estatus', 20);
+            $table->integer('user_id')->unsigned();
+            $table->decimal('tipo_cambio', 9, 6);
+            $table->string('moneda', 10);
+            $table->string('tipo_orden', 20);
+            $table->timestamps();
+            $table->softDeletes();
+        });
 
-		Schema::create('articulos', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->integer('req_id')->unsigned();
-			$table->foreign('req_id')->references('id')->on('reqs');
-			$table->text('articulo');
-			$table->double('cantidad', 12, 5);
-			$table->tinyInteger('impuesto');
-			$table->decimal('monto', 15, 3);
-			$table->integer('oc_id')->unsigned();
-			$table->string('unidad', 20);
-			$table->boolean('inventariable');
+        Schema::create('articulos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('req_id')->unsigned();
+            $table->foreign('req_id')->references('id')->on('reqs');
+            $table->text('articulo');
+            $table->double('cantidad', 12, 5);
+            $table->tinyInteger('impuesto');
+            $table->decimal('monto', 15, 3);
+            $table->integer('oc_id')->unsigned();
+            $table->string('unidad', 20);
+            $table->boolean('inventariable');
             $table->string('obs', 20);
-			$table->timestamps();
-			$table->softDeletes();
-		});
+            $table->timestamps();
+            $table->softDeletes();
+        });
 
-		Schema::create('articulo_rm', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->integer('articulo_id')->unsigned();
-			$table->integer('rm_id')->unsigned();
-			$table->decimal('monto', 15, 3);
-		});
+        Schema::create('articulo_rm', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('articulo_id')->unsigned();
+            $table->integer('rm_id')->unsigned();
+            $table->decimal('monto', 15, 3);
+        });
 
-		Schema::create('cotizaciones', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->integer('req_id')->unsigned();
-			$table->foreign('req_id')->references('id')->on('reqs');
+        Schema::create('cotizaciones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('req_id')->unsigned();
+            $table->foreign('req_id')->references('id')->on('reqs');
             $table->integer('cuadro_id')->unsigned();
             //$table->foreign('cuadro_id')->references('id')->on('cuadros');
-			$table->integer('benef_id')->unsigned();
-			$table->foreign('benef_id')->references('id')->on('benefs');
-			$table->date('fecha_invitacion');
-			$table->date('fecha_cotizacion');
-			$table->string('vigencia', 30);
-			$table->string('garantia', 30);
+            $table->integer('benef_id')->unsigned();
+            $table->foreign('benef_id')->references('id')->on('benefs');
+            $table->date('fecha_invitacion');
+            $table->date('fecha_cotizacion');
+            $table->string('vigencia', 30);
+            $table->string('garantia', 30);
             $table->boolean('imprimir');
-			$table->timestamps();
-			$table->softDeletes();
-		});
+            $table->timestamps();
+            $table->softDeletes();
+        });
 
-		Schema::create('articulo_cotizacion', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->integer('cotizacion_id')->unsigned();
-			$table->foreign('cotizacion_id')->references('id')->on('cotizaciones');
-			$table->integer('articulo_id')->unsigned();
-			$table->foreign('articulo_id')->references('id')->on('articulos');
-			$table->double('costo', 12, 3);
-			$table->boolean('sel');
-			$table->timestamps();
-			$table->softDeletes();
-		});
+        Schema::create('articulo_cotizacion', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('cotizacion_id')->unsigned();
+            $table->foreign('cotizacion_id')->references('id')->on('cotizaciones');
+            $table->integer('articulo_id')->unsigned();
+            $table->foreign('articulo_id')->references('id')->on('articulos');
+            $table->double('costo', 12, 3);
+            $table->boolean('sel');
+            $table->timestamps();
+            $table->softDeletes();
+        });
 
-		Schema::create('cuadros', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->integer('req_id')->unsigned();
-			$table->foreign('req_id')->references('id')->on('reqs');
-			$table->date('fecha_cuadro');
-			$table->string('estatus', 20);
-			$table->smallInteger('elabora')->unsigned();
-			$table->smallInteger('revisa')->unsigned();
-			$table->smallInteger('autoriza')->unsigned();
+        Schema::create('cuadros', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('req_id')->unsigned();
+            $table->foreign('req_id')->references('id')->on('reqs');
+            $table->date('fecha_cuadro');
+            $table->string('estatus', 20);
+            $table->smallInteger('elabora')->unsigned();
+            $table->smallInteger('revisa')->unsigned();
+            $table->smallInteger('autoriza')->unsigned();
             $table->text('criterio_adj');
-			$table->timestamps();
-			$table->softDeletes();
-		});
+            $table->timestamps();
+            $table->softDeletes();
+        });
 
-		Schema::create('ocs', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->integer('req_id')->unsigned();
-			$table->foreign('req_id')->references('id')->on('reqs');
-			$table->integer('oc')->unsigned()->unique();
-			$table->date('fecha_oc');
-			$table->integer('benef_id')->unsigned();
-			$table->foreign('benef_id')->references('id')->on('benefs');
-			$table->string('estatus', 20);
-			$table->timestamps();
-			$table->softDeletes();
-		});
+        Schema::create('ocs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('req_id')->unsigned();
+            $table->foreign('req_id')->references('id')->on('reqs');
+            $table->integer('oc')->unsigned()->unique();
+            $table->date('fecha_oc');
+            $table->integer('benef_id')->unsigned();
+            $table->foreign('benef_id')->references('id')->on('benefs');
+            $table->string('estatus', 20);
+            $table->timestamps();
+            $table->softDeletes();
+        });
 
-		Schema::create('ocs_condiciones', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->integer('oc_id')->unsigned();
-			$table->foreign('oc_id')->references('id')->on('ocs');
-			$table->string('forma_pago', 20);
-			$table->string('fecha_entrega', 20);
-			$table->string('pago', 50);
-			$table->tinyInteger('no_parcialidades');
-			$table->tinyInteger('porcentaje_anticipo');
-			$table->string('fecha_inicio', 20);
-			$table->string('fecha_conclusion', 20);
-			$table->string('fianzas', 12);
-			$table->string('obs');
-		});
+        Schema::create('ocs_condiciones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('oc_id')->unsigned();
+            $table->foreign('oc_id')->references('id')->on('ocs');
+            $table->string('forma_pago', 20);
+            $table->string('fecha_entrega', 20);
+            $table->string('pago', 50);
+            $table->tinyInteger('no_parcialidades');
+            $table->tinyInteger('porcentaje_anticipo');
+            $table->string('fecha_inicio', 20);
+            $table->string('fecha_conclusion', 20);
+            $table->string('fianzas', 12);
+            $table->string('obs');
+        });
 
-		Schema::create('egreso_oc', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->integer('oc_id')->unsigned();
-			$table->integer('egreso_id')->unsigned();
-		});
+        Schema::create('egreso_oc', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('oc_id')->unsigned();
+            $table->integer('egreso_id')->unsigned();
+            $table->foreign('egreso_id')->references('id')->on('egresos')->onDelete('cacade');
+        });
 
-		Schema::create('unidades', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->string('unidad', 50);
-			$table->string('tipo', 30);
-		});
-	}
+        Schema::create('unidades', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('unidad', 50);
+            $table->string('tipo', 30);
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('unidades');
-		Schema::drop('egreso_oc');
-		Schema::drop('ocs_condiciones');
-		Schema::drop('ocs');
-		Schema::drop('cuadros');
-		Schema::drop('articulo_cotizacion');
-		Schema::drop('cotizaciones');
-		Schema::drop('articulo_rm');
-		Schema::drop('articulos');
-		Schema::drop('reqs');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('unidades');
+        Schema::drop('egreso_oc');
+        Schema::drop('ocs_condiciones');
+        Schema::drop('ocs');
+        Schema::drop('cuadros');
+        Schema::drop('articulo_cotizacion');
+        Schema::drop('cotizaciones');
+        Schema::drop('articulo_rm');
+        Schema::drop('articulos');
+        Schema::drop('reqs');
+    }
 
 }
